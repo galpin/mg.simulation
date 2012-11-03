b4038b078c8b868cae30b2025bb706613356d255
Martin Galpin <galpin@gmail.com>
2012-11-03 17:57:49 +0000

    Adds a Guard.IsInRange method.

--- Basics.NET.Tests/GuardTests.cs
+++ Basics.NET.Tests/GuardTests.cs
@@ -1,4 +1,19 @@
-﻿// Copyright (c) Sahara Force India Formula One Team 2012.
+﻿// Basics.NET
+//
+// Copyright (c) Martin Galpin 2012.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library. If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using Xunit;
@@ -10,31 +25,35 @@ namespace Basics.NET.Tests
         #region Public Methods
 
         [Fact]
-        public void IsNotNull_WithExpression_ThrowsIfParameterIsNull_Test()
+        public void IsNotNull_ThrowsIfParameterIsNull_Test()
         {
             object obj = null;
             Assert.Throws<ArgumentNullException>(() => Guard.IsNotNull(obj, () => obj));
-        }
-
-        [Fact]
-        public void IsNotNull_WithExpression_DoesNotThrowIfParameterIsNotNull_Test()
-        {
-            var obj = new object();
-            Assert.DoesNotThrow(() => Guard.IsNotNull(obj, () => obj));
-        }
-
-        [Fact]
-        public void IsNotNull_WithString_ThrowsIfParameterIsNull_Test()
-        {
-            object obj = null;
             Assert.Throws<ArgumentNullException>(() => Guard.IsNotNull(obj, "obj"));
         }
 
         [Fact]
-        public void IsNotNull_WithString_DoesNotThrowIfParameterIsNotNull_Test()
+        public void IsNotNull_DoesNotThrowIfParameterIsNotNull_Test()
         {
             var obj = new object();
+            Assert.DoesNotThrow(() => Guard.IsNotNull(obj, () => obj));
             Assert.DoesNotThrow(() => Guard.IsNotNull(obj, "obj"));
+        }
+
+        [Fact]
+        public void IsInRange_ThrowsIfParameterIsNotWithinRange_Test()
+        {
+            int param = 0;
+            Assert.Throws<ArgumentOutOfRangeException>(() => Guard.IsInRange(param > 0, () => param));
+            Assert.Throws<ArgumentOutOfRangeException>(() => Guard.IsInRange(param > 0, "param"));
+        }
+
+        [Fact]
+        public void IsIsRange_DoesNotThrowIfParameterIsWithinRange_Test()
+        {
+            int param = 10;
+            Assert.DoesNotThrow(() => Guard.IsInRange(param > 0, () => param));
+            Assert.DoesNotThrow(() => Guard.IsInRange(param > 0, "param"));
         }
 
         #endregion
--- Basics.NET/Guard.cs
+++ Basics.NET/Guard.cs
@@ -1,4 +1,19 @@
-﻿// Copyright (c) Sahara Force India Formula One Team 2012.
+﻿// Basics.NET
+//
+// Copyright (c) Martin Galpin 2012.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library. If not, see <http://www.gnu.org/licenses/>. 
 
 using System;
 using System.Diagnostics;
@@ -20,7 +35,7 @@ namespace Basics.NET
         /// <typeparam name="T">The type of <paramref name="obj"/>.</typeparam>
         /// <param name="obj">The object which should not be <see langword="null"/>.</param>
         /// <param name="parameterFunc">
-        /// A function that gets the name of the parameter to check (referenced in the exception message).
+        ///  A function that gets the name of the parameter to check (referenced in the exception message).
         /// </param>
         /// <exception cref="System.ArgumentNullException">
         /// Thrown when <paramref name="obj"/> is <see langword="null"/>.
@@ -46,6 +61,39 @@ namespace Basics.NET
             if (obj == null)
             {
                 throw new ArgumentNullException(parameter);
+            }
+        }
+
+        /// <summary>
+        /// Throws an <see cref="System.ArgumentOutOfRangeException"/> if a specified condition is not <see langword="true"/>.
+        /// </summary>
+        /// <param name="condition">The condition.</param>
+        /// <param name="parameterFunc">
+        /// A function that gets the name of the parameter to check (referenced in the exception message).
+        /// </param>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Thrown when <paramref name="condition"/> is not <see langword="true"/>.
+        /// </exception>
+        public static void IsInRange<T>(bool condition, Expression<Func<T>> parameterFunc)
+        {
+            IsInRange(condition, GetParameterName(parameterFunc));
+        }
+
+        /// <summary>
+        /// Throws an <see cref="System.ArgumentOutOfRangeException"/> if a specified condition is not <see langword="true"/>.
+        /// </summary>
+        /// <param name="condition">The condition.</param>
+        /// <param name="parameter">
+        /// The name of the parameter to check (referenced in the exception message).
+        /// </param>
+        /// <exception cref="System.ArgumentOutOfRangeException">
+        /// Thrown when <paramref name="condition"/> is not <see langword="true"/>.
+        /// </exception>
+        public static void IsInRange(bool condition, string parameter)
+        {
+            if (!condition)
+            {
+                throw new ArgumentOutOfRangeException(parameter);
             }
         }
 
