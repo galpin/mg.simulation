82e22a074de774dfd7f6b73adb2559a00a4932f6
Martin Galpin <martin.galpin@forceindiaf1.com>
2013-05-11 10:28:45 +0100

    Adds an Event type.

---
+++ Simulate.NET.Tests/Events/EventTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Sahara Force India Formula One Team 2013.
+
+using System;
+using Xunit;
+
+namespace Simulate.Events
+{
+    public class EventTests
+    {
+        #region Public Methods
+
+        [Fact]
+        public void Ctor_CorrectlyInitialisesMembers_Test()
+        {
+            var expectedGeneratedOn = DateTime.UtcNow;
+
+            var actual = new StubEvent(expectedGeneratedOn);
+
+            Assert.Equal(expectedGeneratedOn, actual.GeneratedOn);
+        }
+
+        #endregion
+
+        #region StubEvent
+
+        private sealed class StubEvent : Event
+        {
+            public StubEvent(DateTime generatedOn) : base(generatedOn)
+            {
+            }
+        }
+
+        #endregion
+    }
+}
