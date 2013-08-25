b921dcec44cea07949ffd1f8445b51970fe9b2d1
Martin Galpin <martin.galpin@forceindiaf1.com>
2013-08-25 14:07:33 +0100

    Adds tests for SimulationResult.

---
+++ src/Simulate.Net.Tests/SimulationResultTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Sahara Force India Formula One Team 2013.
+
+using System;
+using Xunit;
+
+namespace Simulate
+{
+    public class SimulationResultTests
+    {
+        #region Public Methods
+
+        [Fact]
+        public void GivenCtor_WhenNullSimulationEnvironment_ThenThrows_Test()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SimulationResult<SimulationEnvironment>(null));
+        }
+
+        [Fact]
+        public void GivenCtor_WhenSimulationEnvironment_Then()
+        {
+            var expected = new SimulationEnvironment();
+
+            var result = new SimulationResult<SimulationEnvironment>(expected);
+
+            Assert.Equal(expected, result.Environment);
+        }
+
+        #endregion
+    }
+}
